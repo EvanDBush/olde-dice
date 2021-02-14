e58ef03616d66a28c3668426e338db58affc3d8f
Evan Bush <e.bush05@gmail.com>
2021-02-13 19:32:06 -0500

    applies arrays to javascript. uses loops.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,32 @@
 function rollDice () {
-    // var randomNumber1 = Math.floor(Math.random() *6) + 1;
+    var randomArray = [] 
+for (let i=0; i < 6; i++) {
+
+    randomArray.push(Math.floor(Math.random() *6) + 1)
+
+    document.querySelector('.face' + i).setAttribute("src","img/dice" + randomArray [i] + ".svg");
+}
+
+
+}
+
+
+// old function rollDice () {
+//
+// var randomNumber1 = Math.floor(Math.random() *6) + 1;
     // var randomNumber2 = Math.floor(Math.random() *6) + 1;
     // var randomNumber3 = Math.floor(Math.random() *6) + 1;
     // var randomNumber4 = Math.floor(Math.random() *6) + 1;
     // var randomNumber5 = Math.floor(Math.random() *6) + 1;
     // var randomNumber6 = Math.floor(Math.random() *6) + 1;
 
-var randomArray = [] 
-
-for (let i=0; i < 6; i++) {
-    randomArray.push(Math.floor(Math.random() *6) + 1)
-}
-
-    document.querySelector('.uno').setAttribute("src","img/dice" + randomArray [0] + ".svg");
-    document.querySelector('.dos').setAttribute("src","img/dice" + randomArray [1] + ".svg");
-    document.querySelector('.tres').setAttribute("src","img/dice" + randomArray [2] + ".svg");
-    document.querySelector('.quatro').setAttribute("src","img/dice" + randomArray [3] + ".svg");
-    document.querySelector('.cinco').setAttribute("src","img/dice" + randomArray [4] + ".svg");
-    document.querySelector('.seis').setAttribute("src","img/dice" + randomArray [5] + ".svg");
-}
-console.log(randomArray);
-
+    // document.querySelector('.uno').setAttribute("src","img/dice" + randomArray [0] + ".svg");
+    // document.querySelector('.dos').setAttribute("src","img/dice" + randomArray [1] + ".svg");
+    // document.querySelector('.tres').setAttribute("src","img/dice" + randomArray [2] + ".svg");
+    // document.querySelector('.quatro').setAttribute("src","img/dice" + randomArray [3] + ".svg");
+    // document.querySelector('.cinco').setAttribute("src","img/dice" + randomArray [4] + ".svg");
+    // document.querySelector('.seis').setAttribute("src","img/dice" + randomArray [5] + ".svg");
+    // }
 
 
 // Get 6  random numbers in console
