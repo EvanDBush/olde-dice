f3afa07bb4dd74137baa18cb1678c0de27502683
Evan Bush <e.bush05@gmail.com>
2021-02-19 10:42:19 -0500

    javascript attempts at toggle button

--- scripts.js
+++ scripts.js
@@ -1,10 +1,4 @@
-
 var randomArray = []
-var heldDice = []
-
-const hold = document.querySelector("div" + 'class= "dice"') 
-
-hold.addEventListener('click', toggle)
 
 function rollDice () {
     var randomArray = [] 
@@ -16,46 +10,27 @@ for (let i=0; i < 6; i++) {
 }
 }
 
-    
 
 
 
-// old function rollDice () {
-//
-// var randomNumber1 = Math.floor(Math.random() *6) + 1;
-    // var randomNumber2 = Math.floor(Math.random() *6) + 1;
-    // var randomNumber3 = Math.floor(Math.random() *6) + 1;
-    // var randomNumber4 = Math.floor(Math.random() *6) + 1;
-    // var randomNumber5 = Math.floor(Math.random() *6) + 1;
-    // var randomNumber6 = Math.floor(Math.random() *6) + 1;
-
-    // document.querySelector('.uno').setAttribute("src","img/dice" + randomArray [0] + ".svg");
-    // document.querySelector('.dos').setAttribute("src","img/dice" + randomArray [1] + ".svg");
-    // document.querySelector('.tres').setAttribute("src","img/dice" + randomArray [2] + ".svg");
-    // document.querySelector('.quatro').setAttribute("src","img/dice" + randomArray [3] + ".svg");
-    // document.querySelector('.cinco').setAttribute("src","img/dice" + randomArray [4] + ".svg");
-    // document.querySelector('.seis').setAttribute("src","img/dice" + randomArray [5] + ".svg");
-    // }
 
 
-// Get 6  random numbers in console
-// 
-// function getRandomDice () {
-//     const randomDice = Math.floor(Math.random() *6) + 1;
-//     return randomDice
-// } 
+// attempt 2
+// const a= document.querySelector(".dice");
+// const b= document.querySelector(".hold");
 
-// for ( let i = 0; i < 6; i++) {
-//     console.log( getRandomDice());
+// function hold(a,b) {
+//    for document.querySelector(".dice") {
+//         setAttribute("class", "hold");
+//     }
 // }
 
 
+// attempt 1
 
-// Lesson Dice Roller
-
-// function getRandomNumber () {
-//     const randomNumber = Math.floor( Math.random() * 6 ) + 1;
-//     return randomNumber;
+// function toggle (a,b) {
+//     const a= document.querySelector(.dice")
+//     const b= document.querySelector(.hold)
+//     if a = b;
+//     if b = a;
 // }
-
-// const dieRoll = getRandomNumber (); 
